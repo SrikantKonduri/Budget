f48b2f6bb6fb8140fe1c4a1368c7e4335870e2bc
= <=>
2019-09-01 11:39:53 +0530

    Acc Creation without server interaction

---
+++ src/CreateAcc.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class CreateAcc extends Component{
+    create_acc_handler(){
+        const username = this.refs.uname.value;
+        const password = this.refs.pwd.value;
+        const confirmPassword = this.refs.cpwd.value;
+        const credentials = {
+            username,password
+        }
+        console.log(username,password);
+        if(password === confirmPassword){
+            fetch('http://localhost:8000/signup',{
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(credentials)
+            }).then(res => {
+                console.log(`Got Response: ${res}`);
+            }).catch(err => {
+                console.log(`Caught error: ${err}`);
+            })
+        }
+        else{
+            alert('Passwords do not match');
+        }
+    }
+    render(){
+        return(
+            <div id="frame">
+                <form> 
+                    <h2 id = "app-title">Budget</h2>
+                    <input type = "email" placeholder = 'Username' className = "input-field" id = "uname" ref = 'uname'>
+                    </input>
+                    <input type="password" placeholder="Password" className = "input-field" ref = 'pwd'>
+                    </input>
+                    <input type="password" placeholder="Confirm Password"
+                        className = "input-field" ref = 'cpwd'>
+                    </input>
+                    <button id = 'signup-btn' onClick = {() => this.create_acc_handler()}>Sign Up</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default CreateAcc;
