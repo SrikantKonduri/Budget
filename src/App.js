8f6701cea1e92f42249e61dec8ec037e8e7665ca
= <=>
2019-09-22 12:39:39 +0530

    Implementing login and getting the token from the server, after successful login redirecting to home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import CreateAcc from './CreateAcc'
 import BudgetMain from './BudgetMain'
 import './style.css'
+import {Redirect} from 'react-router-dom';
+
+const jwt = require('jsonwebtoken');
+const {promisify} = require('util');
+const SECRET = 'THIS-IS-MY-SECRET-KEY-YOU-CANT-CRACK-IT';
 
 class App extends Component{
     constructor(props){
@@ -11,6 +16,10 @@ class App extends Component{
             mainPage : true,
             budgetMain: false 
         }
+    }
+    async extractId(token){
+        const decodedData = await promisify(jwt.verify)(token,SECRET);
+        return decodedData.id;
     }
     async login_handler(){
         console.log('handling login')
@@ -32,9 +41,21 @@ class App extends Component{
             const response_server = await res.json();
             console.log(`Did server recieve data?: ${response_server.serverRes}`,response_server);
             if(response_server.serverRes === 'success'){
+                const user_id = await this.extractId(response_server.token);
+                console.log(`user_id: ${user_id}`);
                 this.setState({
-                    budgetMain: true,
-                });
+                    loggedIn: true,
+                    token: response_server.token
+                })
+                // <Redirect to = {`/home/${response_server.token}`}/>
+                // this.props.history.push(`/home/${response_server.token}`);
+                // this.props.history.push({
+                //     pathname: '/home',
+                //     state: {token: response_server.token}
+                // })
+            }
+            else{
+                alert('Invalid username or password');
             }
         }
     }
@@ -46,29 +67,32 @@ class App extends Component{
     render(){
         var render_loginApp = 
         <div id="frame">
-            <form> 
-                <h2 id = "app-title">Budget</h2>
-                <input type = "email" placeholder = 'Username' className = "input-field" id = "uname" ref = 'uname'>
-                </input>
-                <input type="password" placeholder="Password" className = "input-field" ref = 'pwd'>
-                </input>
-                <div id = 'buttons'>
-                    <button href = '' id = 'create-acc-btn' 
-                     className = 'btn' onClick = {()=>this.createAcc_handler()}>
-                        Create account
-                    </button>
-                    <button id = 'login-btn'
-                     className = 'btn' onClick = {()=>this.login_handler()}>
-                        Login
-                    </button>
-                </div>
-            </form>
+            <h2 id = "app-title">Budget</h2>
+            <input type = "email" placeholder = 'Username' className = "input-field" id = "uname" ref = 'uname'>
+            </input>
+            <input type="password" placeholder="Password" className = "input-field" ref = 'pwd'>
+            </input>
+            <div id = 'buttons'>
+                <button href = '' id = 'create-acc-btn' 
+                    className = 'btn' onClick = {()=>this.createAcc_handler()}>
+                    Create account
+                </button>
+                <button id = 'login-btn'
+                    className = 'btn' onClick = {()=>this.login_handler()}>
+                    Login
+                </button>
+            </div>            
         </div>
         var render_createApp = <CreateAcc />
-        var render_budgetMain = <BudgetMain />
-        if(this.state.budgetMain){
+        // var render_budgetMain = <BudgetMain />
+        if(this.state.loggedIn){
             console.log('rendering budget..');
-            return(render_budgetMain);          
+            return <Redirect 
+                    to = {{
+                        pathname: `/home`,
+                        state: {token: this.state.token}
+                    }}/>
+            // return <Redirect to = {`/home/${this.state.token}`}/>
         }
         else if(this.state.mainPage){
             console.log('Huahhhhh');
